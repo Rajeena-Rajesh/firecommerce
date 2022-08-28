1c21e901e40512bfdb9edd3d9fe1bb2b2f188c25
rajeena-rajesh <rajeenarajesh1@gmail.com>
2022-08-28 11:30:48 +0530

    firecommerce-products added

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,23 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Layout from '../Components/Layout'
-import {collection,addDoc} from "firebase/firestore"
+import {collection,getDocs} from "firebase/firestore"
 import fireDB from '../fireconfig';
+//import { fireproducts } from '../firecommerce-products';
 
  
 
 
 function Homepage() {
+const[products,setProducts]=useState([])
+ useEffect(()=>{
+  getData()
+ },[])
 
-  async function addData(){
-    const docRef = await addDoc(collection(fireDB, "users"), {
-      name: "Rajeena",
-      age: 33
-    });
-       } 
+       async function getData(){
+        try {
+        
+          const users=await getDocs(collection(fireDB, "products"));
+const productsArray=[]
+users.forEach((doc) => {
+  
+  const obj={
+    id:doc.id ,
+    ...doc.data()
+  }
+  productsArray.push(obj)
+});
+ setProducts(productsArray)
+        } catch (error) {
+         console.log(error)
+        }
+          } 
   return (
     <Layout>
       <h4>homepage</h4>
-        <button onClicl={addData}>Add data to firebase</button>
+        
+        
     </Layout>
   )
 }
